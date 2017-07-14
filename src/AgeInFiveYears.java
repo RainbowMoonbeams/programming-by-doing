dadb70b984952e01a2ebd0fe4ed5d763e0835c84
Meaghan <meaghan.rea@gmail.com>
2017-07-13 22:04:35 -0400

    edits

--- src/AgeInFiveYears.java
+++ src/AgeInFiveYears.java
@@ -4,27 +4,23 @@ public class AgeInFiveYears {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		// Ask the user for their name. Then display their name to prove that you can recall it. 
-		// Ask them for their age. Then display what their age would be five years from now. 
+		// Ask the user for their name. Then display their name to prove that you can
+		// recall it.
+		// Ask them for their age. Then display what their age would be five years from
+		// now.
 		// Then display what their age would be five years ago.
-		
+
 		System.out.println("What is your name?");
 		String name = input.nextLine();
-		
+
 		System.out.println(name + ", what is your age?");
 		int age = input.nextInt();
-		
+
 		System.out.println("Did you know that in five years, you'll be " + (age + 5) + "?? Well, math proves that.");
-		
-		
-		System.out.println("And five years ago, you were " + (age - 5) + ". You don't look a day over " + (age -3) + ".");
-		
-		
-		System.out.println("You don't look a day over " + (age -3) + ".");
-		
-		
-		
-		
+
+		System.out.println(
+				"And five years ago, you were " + (age - 5) + ". You don't look a day over " + (age - 3) + ".");
+
 	}
 
 }
